5dd047a1c600aef2a73f6904866a52b99e669a9d
AmirGhz17 <118483172+AmirGhz17@users.noreply.github.com>
2024-05-18 11:28:27 +0330

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,47 +1,51 @@
 var user = 0;
 var ai = 0;
 
-set();
 
 function set() {
     let u = document.getElementById("pScore");
     let a = document.getElementById("aiScore");
+
 
     u.innerHTML = user;
     a.innerHTML = ai;
 }
 
 function check() {
-    let o = document.querySelector('input[name="run"]:checked').value;
+    let u = document.querySelector('input[name="run"]:checked').value;
     console.log(u);
-    const choises = ["paper", "scisssors", "rock"];
-    let a = choises[Math, floor(Math.random() * choises.length)];
+    const choises = ["Paper", "Scissors", "Rock"];
+    let a = choises[Math.floor(Math.random() * choises.length)];
+    console.log(a);
 
-    let c = document.getElementById("comp");
-    if (a == 0) {
-        c.innerHTML = "Paper";
-    } else if (a == 1) {
-        c.innerHTML = "Scssors";
-    } else {
-        c.innerHTML = "Rock"
-    }
+    // let c = document.getElementById("comp");
+    // if (a == 0) {
+    //     c.innerHTML = "Paper";
+    // } else if (a == 1) {
+    //     c.innerHTML = "Scissors";
+    // } else {
+    //     c.innerHTML = "Rock"
+    // }
 
-    if (o == a) {
-        r = 0;
+    if (u == a) {
+        r(0, a);
     } else if (
-        (o == 0 && a == 2) ||
-        (o == 1 && a == 0) ||
-        (o == 2 && a == 1)) {
-        user;
-        r(1);
-    } else {
+        (u == "Paper" && a == "Scissors") ||
+        (u == "Scissors" && a == "Rock") ||
+        (u == "Rock" && a == "Paper")) {
         ai++;
-        r(-1);
+        r(-1, a);
+    } else {
+        user++;
+        r(1, a);
     }
 }
 
-function r(s) {
+function r(s, u) {
     let t = document.getElementById("one");
+    let y = document.getElementById("pScore");
+    let x = document.getElementById("aiScore");
+    let e = document.getElementById("comp");
     if (s == -1) {
         t.innerHTML = "You Lost!";
         t.style.color = "red";
@@ -50,6 +54,11 @@ function r(s) {
         t.style.color = "green";
     } else {
         t.innerHTML = "DRAW!";
+        t.style.color = "white";
     }
-
+    y.innerHTML = user;
+    x.innerHTML = ai;
+    e.innerHTML = u;
 }
+
+set();
